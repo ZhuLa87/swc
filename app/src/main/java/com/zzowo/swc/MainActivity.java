228bb89cc429f70760281bd3cff7ba8763a0dd80
ZzOwO <46343155+JImmy0096@users.noreply.github.com>
2023-07-03 04:17:39 +0800

    建立底部導覽欄

---
+++ app/src/main/java/com/zzowo/swc/MainActivity.java
@@ -0,0 +1,45 @@
+package com.zzowo.swc;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.zzowo.swc.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+//        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES); // Night mode is enable by default
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.bottom_home) {
+                replaceFragment(new HomeFragment());
+            } else if (itemId == R.id.bottom_map) {
+                replaceFragment(new MapFragment());
+            } else if (itemId == R.id.bottom_notifications) {
+                replaceFragment(new NotificationsFragment());
+            } else if (itemId == R.id.bottom_settings) {
+                replaceFragment(new SettingsFragment());
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+}
