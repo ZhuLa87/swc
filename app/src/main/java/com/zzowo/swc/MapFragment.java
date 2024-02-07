b76179b9f8831cae0ea718c6e5ceafd078c740dc
ZzOwO <46343155+JImmy0096@users.noreply.github.com>
2024-02-08 04:08:48 +0800

    主使用者地圖頁面取得位置權限, 移動至當前位置 Known issue: setMyLocationButtonEnabled按鈕遺失

--- app/src/main/java/com/zzowo/swc/MapFragment.java
+++ app/src/main/java/com/zzowo/swc/MapFragment.java
@@ -1,13 +1,48 @@
 package com.zzowo.swc;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.location.Location;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,6 +52,15 @@ import android.widget.ProgressBar;
 public class MapFragment extends Fragment {
     private static final String TAG = "MapFragment";
     ProgressBar progressBar;
+    Boolean isLocationPermissionGranted = false;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    SupportMapFragment supportMapFragment;
+    private GoogleMap map;
+    private Location lastKnownLocation;
+    // A default location (Sydney, Australia) and default zoom to use when location permission is
+    // not granted.
+    private final LatLng defaultLocation = new LatLng(-33.8523341, 151.2106085);
+    private static final float DEFAULT_ZOOM = 17.5F;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -65,6 +109,156 @@ public class MapFragment extends Fragment {
 
         progressBar = rootView.findViewById(R.id.progressBar);
 
+        // init
+        initStatusBarColor();
+
+        // TODO: 判斷是否為主使用者
+
+        // 建立 SupportMapFragment 物件
+        supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        // 建立 FusedLocationProviderClient 物件
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+
+        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull GoogleMap googleMap) {
+                map = googleMap;
+                Log.d(TAG, "onMapReady: " + googleMap.toString());
+
+                // permission check
+                grantPermission();
+
+            }
+        });
+
         return rootView;
+    }
+
+    private void initStatusBarColor() {
+        Window window = getActivity().getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(getActivity().getResources().getColor(R.color.colorSurface));
+    }
+
+    private void grantPermission() {
+        Log.d(TAG, "Checking permissions...");
+        Dexter.withContext(getActivity())
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override public void onPermissionGranted(PermissionGrantedResponse response) {
+                        // permission is granted
+                        Log.d(TAG, "Permission granted: ACCESS_FINE_LOCATION");
+                        isLocationPermissionGranted = true;
+
+                        // Turn on the My Location layer and the related control on the map.
+                        updateLocationUI();
+
+                        // Get the current location of the device and set the position of the map.
+                        getDeviceLocation();
+                    }
+                    @Override public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Log.d(TAG, "Permission denied: ACCESS_FINE_LOCATION");
+                        showSettingsDialog();
+                        Toast.makeText(getActivity(), R.string.location_perm_denied_msg, Toast.LENGTH_SHORT).show();
+                    }
+                    @Override public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken permissionToken) {
+                        Log.d(TAG, "Permission rationale should be shown: ACCESS_FINE_LOCATION");
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
+        map.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
+            @Override
+            public void onMapLoaded() {
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    private void showSettingsDialog() {
+        // we are displaying an alert dialog for permissions
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        // below line is the title for our alert dialog.
+        builder.setTitle("Need Permissions");
+
+        // below line is our message for our dialog
+        builder.setMessage("This app needs permission to use this feature. You can grant them in app settings.");
+        builder.setPositiveButton("GOTO SETTINGS", (dialog, which) -> {
+            // this method is called on click on positive button and on clicking shit button
+            // we are redirecting our user from our app to the settings page of our app.
+            dialog.cancel();
+            // below is the intent from which we are redirecting our user.
+            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+            Uri uri = Uri.fromParts("package", getActivity().getPackageName(), null);
+            intent.setData(uri);
+            startActivityForResult(intent, 101);
+        });
+        builder.setNegativeButton("Cancel", (dialog, which) -> {
+            // this method is called when user click on negative button.
+            dialog.cancel();
+        });
+        // below line is used to display our dialog
+        builder.show();
+    }
+
+
+    @SuppressLint("MissingPermission")
+    private void updateLocationUI(){
+        Log.d(TAG, "update location UI");
+        if (map == null) {
+            Log.d(TAG, "Map is empty");
+            return;
+        }
+        try {
+            if (isLocationPermissionGranted) {
+                Log.d(TAG, "Showing my location UI on map...");
+                // 在地圖上啟用「我的位置」圖層和相關控制項
+                map.setMyLocationEnabled(true); // 顯示藍色的圓形圖標，表示使用者的位置
+                map.getUiSettings().setMyLocationButtonEnabled(true); // 顯示「我的位置」按鈕
+            } else {
+                // 停用並將目前位置設為空值
+                map.setMyLocationEnabled(false);
+                map.getUiSettings().setMyLocationButtonEnabled(false);
+                lastKnownLocation = null;
+                grantPermission();
+            }
+        } catch (SecurityException e) {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getDeviceLocation() {
+        progressBar.setVisibility(View.VISIBLE);
+        try {
+            if (isLocationPermissionGranted) {
+                Log.d(TAG, "Getting device location...");
+                Task<Location> locationResult = fusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful()) {
+                            // Set the map's camera position to the current location of the device.
+                            lastKnownLocation = task.getResult();
+                            if (lastKnownLocation != null) {
+                                Log.d(TAG, "Last known location: " + lastKnownLocation.getLatitude() + ", " + lastKnownLocation.getLongitude());
+                                map.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                        new LatLng(lastKnownLocation.getLatitude(),
+                                                lastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+                            }
+                        } else {
+                            Log.d(TAG, "Current location is null. Using defaults.");
+                            Log.e(TAG, "Exception: %s", task.getException());
+                            map.animateCamera(CameraUpdateFactory
+                                    .newLatLngZoom(defaultLocation, DEFAULT_ZOOM));
+                            map.getUiSettings().setMyLocationButtonEnabled(false);
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e) {
+            Log.e("Exception: %s", e.getMessage());
+        }
     }
 }
